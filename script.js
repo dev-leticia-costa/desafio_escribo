95744b43fc01fbffd99289ff39c2275274d0e0b3
dev-leticia-costa <leticiaccmarques@hotmail.com>
2023-11-19 23:32:58 -0300

    Create function somatorio

--- script.js
+++ script.js
@@ -1,48 +1,26 @@
-//Implemente uma função que receba um número inteiro positivo 
-//e retorne o somatório de todos os valores inteiros divisíveis por 3 ou 5 
-//que sejam inferiores ao número passado.
-//Exemplos:
-//Caso sua função receba o inteiro 10, ela deve retornar 23,
-// resultante do somatório dos números 3, 5, 6 e 9 que são menores que 10.
-//Caso sua função receba o inteiro 11, ela deve retornar 33, 
-//resultante do somatório dos números 3, 5, 6, 9 e 10 que são menores que 11.
+function somatorio(numero) {
+  // Código da função
+  let input = prompt("Digite um número inteiro positivo:");
+  let soma = 0;
 
-//Implemente uma função que receba um número inteiro 
-//positivo - OK
-//e retorne o somatório de todos os valores inteiros divisíveis por 3 ou 5 
-//que sejam inferiores ao número passado.
-//Exemplos:
-//Caso sua função receba o inteiro 10, ela deve retornar 23,
-// resultante do somatório dos números 3, 5, 6 e 9 que são menores que 10.
-//Caso sua função receba o inteiro 11, ela deve retornar 33, 
-//resultante do somatório dos números 3, 5, 6, 9 e 10 que são menores que 11.
+  // Converter a entrada para um número inteiro - isNan não porque vai entrar como string
+  numero = parseInt(input);
+  if (numero >= 0 && numero % 1 === 0) {
+    for (let i = 1; i < numero; i++) {
+      console.log(`i: ${i}`);
 
-function minhaFuncao(numero) {
-    // Código da função
-        let input = prompt("Digite um número inteiro positivo:");
+      if (i % 3 === 0 || i % 5 === 0) {
+        soma += i;
+      }
+    }
+    console.log(`soma: ${soma}`);
+    //alert("número inteiro positivo válido.");
+  } else {
+    alert("Por favor, digite um número inteiro positivo válido.");
+  }
 
-        // Converter a entrada para um número inteiro - isNan não porque vai entrar como string
-        numero = parseInt(input);
-        if (
-           numero >= 0 && numero % 1 === 0){
-            //percorrer enquanto o contador for menor que o número
-              for (let i = 1; i < numero; i++) {
-
-               console.log(`i: ${i}`);
-                //testar se é divisível
-                if (i % 3 === 0 || i % 5 === 0) {
-                    console.log(`  i é divisível`);
-                } else {
-                    console.log(`  i não é divisível`);
-                }
-}
-           //alert("número inteiro positivo válido.");
-
-        }
-        else {
-            alert("Por favor, digite um número inteiro positivo válido.");
-        }
-
-//    console.log(numero);
+  //    console.log(numero);
 }
 //minhaFuncao()
+//tratar erros: voltar pra o início do código para inserir novamente
+//criar outra função

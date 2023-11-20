6c63f18b940f5ca711b3c2d55099525ec27f96ad
dev-leticia-costa <leticiaccmarques@hotmail.com>
2023-11-19 23:42:28 -0300

    Refactoring

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-function somatorio(numero) {
-  // Código da função
+function minhaFuncao(numero) {
   let input = prompt("Digite um número inteiro positivo:");
-  let soma = 0;
-
-  // Converter a entrada para um número inteiro - isNan não porque vai entrar como string
   numero = parseInt(input);
+  
   if (numero >= 0 && numero % 1 === 0) {
-    for (let i = 1; i < numero; i++) {
-      console.log(`i: ${i}`);
-
-      if (i % 3 === 0 || i % 5 === 0) {
-        soma += i;
-      }
-    }
-    console.log(`soma: ${soma}`);
-    //alert("número inteiro positivo válido.");
+    return somatorio(numero);
   } else {
     alert("Por favor, digite um número inteiro positivo válido.");
+    input = prompt("Digite um número inteiro positivo:");
+    numero = parseInt(input);
+    if (numero >= 0 && numero % 1 === 0) {
+      return somatorio(numero);
+    } else {
+      alert("Por favor, digite um número inteiro positivo válido.");
+    }
   }
-
-  //    console.log(numero);
 }
-//minhaFuncao()
 //tratar erros: voltar pra o início do código para inserir novamente
-//criar outra função
+function somatorio(numero) {
+  let soma = 0;
+  for (let i = 1; i < numero; i++) {
+    console.log(`i: ${i}`);
+
+    if (i % 3 === 0 || i % 5 === 0) {
+      soma += i;
+    }
+    console.log(`soma: ${soma}`);
+  }
+}
+minhaFuncao();
+
+function validaNumero(){
+if (numero >= 0 && numero % 1 === 0) {
+    return somatorio(numero);
+  } else {
+    alert("Por favor, digite um número inteiro positivo válido.");
+    input = prompt("Digite um número inteiro positivo:");
+    numero = parseInt(input);
+    if (numero >= 0 && numero % 1 === 0) {
+      return somatorio(numero);
+    } else {
+      alert("Por favor, digite um número inteiro positivo válido.");
+    }
+  }
+}
